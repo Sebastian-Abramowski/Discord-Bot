464a8276b48f1d0367439fcc0f139bd4a41d4dbb
Sebastian <sebastian.abramowski@onet.pl>
2023-09-07 23:35:23 +0200

    [R] Refactor of bot.py

--- bot.py
+++ bot.py
@@ -2,35 +2,43 @@ import discord
 from discord.ext import commands
 from discord import app_commands
 import responses
+import dotenv
+import os
+
+dotenv.load_dotenv()
 
 
-async def send_message(message, user_message, is_private):
+async def send_message(message, user_message):
     try:
         response = responses.handle_response(user_message)
-        if is_private:
-            await message.author.send(response)
-        else:
-            await message.channel.send(response)
+        await message.channel.send(response)
     except Exception as e:
         print(e)
 
 
 def run_discord_bot():
-    token = None
-    with open("token.txt", 'r') as file_handle:
-        token = file_handle.read()
-
     bot = commands.Bot(command_prefix=".", intents=discord.Intents.all())
 
     @bot.event
     async def on_ready():
-        print(f"{bot.user} is now running")
+        print(f"[BOT-INFO] {bot.user} is now running")
 
         try:
             synced = await bot.tree.sync()
-            print(f"Synced {len(synced)} command(s)")
+            print(f"[BOT-INFO] Synced {len(synced)} command(s)")
         except Exception as e:
             print(e)
+
+    @bot.event
+    async def on_message(message):
+        if message.author == bot.user:
+            return
+
+        username = str(message.author)
+        user_message = str(message.content)
+        channel = str(message.channel)
+
+        print(f"[USER-INFO] {username} said '{user_message}' on channel {channel}")
 
     @bot.tree.command(name="join", description="join voice channel")
     async def join(interaction: discord.Interaction):
@@ -42,9 +50,9 @@ def run_discord_bot():
                 await voice.move_to(channel)
             else:
                 voice = await channel.connect()
+            await interaction.response.send_message("`Joined or moved to the voice channel.`")
         else:
-            await interaction.response.send_message("You are not in a voice channel.")
-        await interaction.response.send_message("Joined or moved to the voice channel.")
+            await interaction.response.send_message("`You are not in a voice channel.`")
 
     @bot.tree.command(name="hello", description="Get greeted")
     async def hello(interaction: discord.Interaction):
@@ -63,15 +71,4 @@ def run_discord_bot():
                f"to play {arg}")
         await interaction.response.send_message(msg)
 
-    @bot.event
-    async def on_message(message):
-        if message.author == bot.user:
-            return
-
-        username = str(message.author)
-        user_message = str(message.content)
-        channel = str(message.channel)
-
-        print(f"{username} said '{user_message}' on channel {channel}")
-
-    bot.run(token)
+    bot.run(os.getenv('TOKEN'))
