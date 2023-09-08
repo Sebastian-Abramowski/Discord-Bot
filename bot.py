19c822ab58c8798dc94341173c1bbdec8c5511f9
Sebastian <sebastian.abramowski@onet.pl>
2023-09-08 11:44:16 +0200

    Added options to disconnect, stop, resume, pause and small changes in functions for playing and joining

--- bot.py
+++ bot.py
@@ -44,17 +44,31 @@ def run_discord_bot():
 
         print(f"[USER-INFO] {username} said '{user_message}' on channel {channel}")
 
+        for mentioned_member in message.mentions:
+            if mentioned_member.name == "DunkeyBot" and mentioned_member.bot is True:
+                if "?" == message.content[-1]:
+                    await message.reply("I invoke my right not to answer this question")
+                else:
+                    await message.reply("Who asked?")
+
     @bot.tree.command(name="join", description="join voice channel")
     async def join(interaction: discord.Interaction):
         if interaction.user.voice:
             channel = interaction.user.voice.channel
-            voice = interaction.guild.voice_client
+            voice = discord.utils.get(bot.voice_clients, guild=interaction.guild)
 
             if voice and voice.is_connected():
-                await voice.move_to(channel)
+                if voice.channel == channel:
+                    await interaction.response.send_message(
+                        "`The bot is already on your channel`")
+                else:
+                    await voice.move_to(channel)
+                    await interaction.response.send_message(
+                        "`The bot has been moved to your channel`")
             else:
                 voice = await channel.connect()
-            await interaction.response.send_message("`Joined or moved to the voice channel.`")
+                await interaction.response.send_message(
+                    "`The bot has joined your channel`")
         else:
             await interaction.response.send_message("`You are not in a voice channel.`")
 
@@ -75,20 +89,83 @@ def run_discord_bot():
         # VoiceClient associated with the specified guild (there is one bot per server)
         voice = discord.utils.get(bot.voice_clients, guild=guild)
 
-        if voice.is_playing():
-            await interaction.response.send_message("Sth is already playing")
-            return
+        if voice and voice.is_connected():
+            if voice.is_playing():
+                await interaction.response.send_message("Sth is already playing")
+                return
+            else:
+                await interaction.response.send_message("Here will be url info")
+
+                # Use youtube-dl to extract the direct audio URL
+                ydl_opts = {'format': 'bestaudio/best', 'postprocessors': [{'key': 'FFmpegExtractAudio',
+                                                                            'preferredcodec': 'mp3'}]}
+                with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                    info = ydl.extract_info(url, download=False)
+                    url = info['url']  # url to audio, there the audio data is in Opus format
+                    # Opus format - way of compressing and encoding the audio data
+
+                source = await discord.FFmpegOpusAudio.from_probe(url, **FFMPEG_OPTIONS)
+                voice.play(source)
         else:
-            await interaction.response.send_message("Here will be url info")
+            await interaction.response.send_message(
+                "`The bot is not conencted to any channel`")
 
-            # Use youtube-dl to extract the direct audio URL
-            ydl_opts = {'format': 'bestaudio/best', 'postprocessors': [{'key': 'FFmpegExtractAudio',
-                                                                        'preferredcodec': 'mp3'}]}
-            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-                info = ydl.extract_info(url, download=False)
-                url = info['url']  # url to audio
+    @bot.tree.command(name='pause', description='pause the bot audio')
+    async def pause(interaction: discord.Interaction):
+        voice = discord.utils.get(bot.voice_clients, guild=interaction.guild)
 
-            source = await discord.FFmpegOpusAudio.from_probe(url, **FFMPEG_OPTIONS)
-            voice.play(source)
+        if voice and voice.is_connected():
+            if voice.is_playing():
+                voice.pause()
+                await interaction.response.send_message("`The bot has been paused`")
+            else:
+                await interaction.response.send_message("`The bot is currently not playing`")
+        else:
+            await interaction.response.send_message(
+                "`The bot is not conencted to any channel`")
+
+    @bot.tree.command(name='resume', description="resume the bot's audio")
+    async def resume(interaction: discord.Interaction):
+        voice = discord.utils.get(bot.voice_clients, guild=interaction.guild)
+
+        if voice and voice.is_connected():
+            if voice.is_paused():
+                voice.resume()
+                await interaction.response.send_message("`The bot has been resumed`")
+            else:
+                if voice.is_playing():
+                    await interaction.response.send_message("`The bot is currently playing something`")
+                else:
+                    await interaction.response.send_message("`The bot is currently not paused`")
+        else:
+            await interaction.response.send_message(
+                "`The bot is not conencted to any channel`")
+
+    @bot.tree.command(name='stop', description="stop the bot's audio")
+    async def stop(interaction: discord.Interaction):
+        voice = discord.utils.get(bot.voice_clients, guild=interaction.guild)
+
+        if voice and voice.is_connected():
+            if voice.is_playing():
+                voice.stop()
+                await interaction.response.send_message("`The bot has been stopped`")
+            else:
+                await interaction.response.send_message(
+                    "`You cannot stop the bot which is currently not playing`")
+        else:
+            await interaction.response.send_message(
+                "`The bot is not conencted to any channel`")
+
+    @bot.tree.command(name='disconnect', description="disconnect the bot from the channel")
+    async def disconnect(interaction: discord.Interaction):
+        voice = discord.utils.get(bot.voice_clients, guild=interaction.guild)
+
+        if voice and voice.is_connected():
+            await voice.disconnect()
+            await interaction.response.send_message(
+                "`The bot has been disconnect from your channel`")
+        else:
+            await interaction.response.send_message(
+                "`The bot is not currently on any channel`")
 
     bot.run(os.getenv('TOKEN'))
