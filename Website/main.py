9f04f5306dc62e9be959cbcb12a7a955dfacb43d
Sebastian <sebastian.abramowski@onet.pl>
2023-09-28 16:43:11 +0200

    Transition to Flask - home page

---
+++ Website/main.py
@@ -0,0 +1,13 @@
+import flask
+
+app = flask.Flask(__name__)
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return flask.render_template("home.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
